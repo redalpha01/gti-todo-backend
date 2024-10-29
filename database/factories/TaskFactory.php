00a4c97a2366023f2b1f50c489232822c9d7a842
Emanuel Garon <egaron@mbiance.com>
2024-10-29 16:01:15 -0400

    [0.4.0] - Complétion du factory

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -2,10 +2,11 @@
 
 namespace Database\Factories;
 
+use App\Models\Task;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ * @extends Factory<Task>
  */
 class TaskFactory extends Factory
 {
@@ -17,7 +18,8 @@ class TaskFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'description' => $this->faker->sentence(),
+            'is_done'     => $this->faker->boolean(),
         ];
     }
 }
